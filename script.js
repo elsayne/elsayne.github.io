2173f701e5862e6c694d9783375c855d6de2ce54
elsayne <jrdh99@gmail.com>
2025-08-23 23:36:13 +1000

    Make it shiny

---
+++ script.js
@@ -0,0 +1,9 @@
+const leftDiv = document.getElementById('left');
+const rightDiv = document.getElementById('right');
+
+leftDiv.addEventListener('click', () => {
+    window.location.href = '/Maddie/index.html';
+});
+rightDiv.addEventListener('click', () => {
+    window.location.href = '/Andrea/index.html';
+});
